6a23720adee8145e84ae37ee6ab3e1692c3d6cf0
BS0404 <gab0r15@proton.me>
2025-01-19 00:26:51 +0100

    Első feltöltés

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCHh9DY1iZtN63FTuGuzibRqG6stva02j8",
+    authDomain: "doga2025-63897.firebaseapp.com",
+    projectId: "doga2025-63897",
+    storageBucket: "doga2025-63897.firebasestorage.app",
+    messagingSenderId: "900724914047",
+    appId: "1:900724914047:web:e5402ff3ffcb4cd7b8cc4a"
+
+  }
+};
